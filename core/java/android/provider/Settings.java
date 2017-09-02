1503386ee74f22946a6f0f53a848ea5757a82dfc
maxwen <max.weninger@gmail.com>
2017-09-02 14:27:50 +0800

    WakelockBlocker [1/2] ported by @faust93

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4523,6 +4523,18 @@ public final class Settings {
         public static final String BATTERY_BAR_USE_GRADIENT_COLOR = "battery_bar_use_gradient_color";
 
         /**
+         * Enable blocking wakelock
+         * @hide
+         */
+        public static final String WAKELOCK_BLOCKING_ENABLED = "wakelock_blocking_enabled";
+
+         /**
+         * List of wakelock blocks selected
+         * @hide
+         */
+        public static final String WAKELOCK_BLOCKING_LIST = "wakelock_blocking_list";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
