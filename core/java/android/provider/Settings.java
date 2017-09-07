8d8d440adac4f09ab995a6808b86fe1633eceaaf
xyyx <xyyx@mail.ru>
2017-09-07 11:16:04 +0800

    Ability to disable dashboards conditions (1/2)
    Signed-off-by: phenomenall <venkysaihot@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4590,6 +4590,12 @@ public final class Settings {
         public static final String ALARM_BLOCKING_ENABLED = "alarm_blocking_enabled";
 
         /**
+         * Disable dashboard conditions in settings
+         * @hide
+         */
+        public static final String ENABLE_CONDITIONS = "enable_conditions";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
