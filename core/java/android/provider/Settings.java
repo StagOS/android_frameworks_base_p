cf78ca6e43dfa2c52ae6eb2a13fb52ae5ba9d220
Ali B <abittin@gmail.com>
2018-04-06 16:28:25 +0300

    SystemUI: QS footer visibilities [1/2]
    Enable toggling visibilities of running services and
    settings icons in the quicksettings footer.
    
    For settings icon, we are actually hiding entire
    settingscontainer when toggled on. So even if tuner
    is enabled, that will also be hidden.
    
    Change-Id: I2aa356a538d472d3478f071df05bceb93b0055bd

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5852,6 +5852,20 @@ public final class Settings {
                 "notification_guts_kill_app_button";
 
         /**
+         * Show Settings icon in QS Footer
+         *
+         * @hide
+         */
+        public static final String QSFOOTER_SHOW_SETTINGS = "qs_footer_show_settings";
+
+        /**
+         * Show Running Services icon in QS Footer.
+         *
+         * @hide
+         */
+        public static final String QSFOOTER_SHOW_SERVICES = "qs_footer_show_services";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
--- packages/SystemUI/src/com/android/systemui/qs/QSFooterImpl.java
+++ packages/SystemUI/src/com/android/systemui/qs/QSFooterImpl.java
@@ -28,6 +28,7 @@ import android.graphics.drawable.Drawable;
 import android.graphics.drawable.RippleDrawable;
 import android.os.Bundle;
 import android.os.UserManager;
+import android.provider.Settings;
 import android.support.annotation.Nullable;
 import android.support.annotation.VisibleForTesting;
 import android.text.TextUtils;
@@ -61,9 +62,14 @@ import com.android.systemui.statusbar.policy.NetworkController.SignalCallback;
 import com.android.systemui.statusbar.policy.UserInfoController;
 import com.android.systemui.statusbar.policy.UserInfoController.OnUserInfoChangedListener;
 import com.android.systemui.tuner.TunerService;
+import com.android.systemui.tuner.TunerService.Tunable;
 
-public class QSFooterImpl extends FrameLayout implements QSFooter,
-        OnClickListener, OnUserInfoChangedListener, EmergencyListener, SignalCallback {
+public class QSFooterImpl extends FrameLayout implements Tunable, QSFooter,
+        OnClickListener,  OnUserInfoChangedListener,
+        EmergencyListener, SignalCallback {
+
+    private static final String QS_FOOTER_SHOW_SETTINGS = "qs_footer_show_settings";
+    private static final String QS_FOOTER_SHOW_SERVICES = "qs_footer_show_services";
 
     private ActivityStarter mActivityStarter;
     private UserInfoController mUserInfoController;
@@ -131,6 +137,8 @@ public class QSFooterImpl extends FrameLayout implements QSFooter,
         mDragHandle = findViewById(R.id.qs_drag_handle_view);
         mActionsContainer = findViewById(R.id.qs_footer_actions_container);
 
+        updateVisibilities();
+
         // RenderThread is doing more harm than good when touching the header (to expand quick
         // settings), so disable it for this view
         ((RippleDrawable) mSettingsButton.getBackground()).setForceSoftware(true);
@@ -143,6 +151,17 @@ public class QSFooterImpl extends FrameLayout implements QSFooter,
                 oldBottom) -> updateAnimator(right - left));
         setImportantForAccessibility(IMPORTANT_FOR_ACCESSIBILITY_YES);
         updateEverything();
+    }
+
+    @Override
+    protected void onAttachedToWindow() {
+        super.onAttachedToWindow();
+        Dependency.get(TunerService.class).addTunable(this, QS_FOOTER_SHOW_SETTINGS);
+        Dependency.get(TunerService.class).addTunable(this, QS_FOOTER_SHOW_SERVICES);
+    }
+
+    public void onTuningChanged(String key, String newValue) {
+        updateVisibilities();
     }
 
     private void updateAnimator(int width) {
@@ -233,6 +252,7 @@ public class QSFooterImpl extends FrameLayout implements QSFooter,
     @VisibleForTesting
     public void onDetachedFromWindow() {
         setListening(false);
+        Dependency.get(TunerService.class).removeTunable(this);
         super.onDetachedFromWindow();
     }
 
@@ -280,9 +300,14 @@ public class QSFooterImpl extends FrameLayout implements QSFooter,
     private void updateVisibilities() {
         mSettingsContainer.setVisibility(mQsDisabled ? View.GONE : View.VISIBLE);
         final boolean isDemo = UserManager.isDeviceInDemoMode(mContext);
+        boolean servicesButtonVisible = Settings.System.getInt(mContext.getContentResolver(),
+                Settings.System.QSFOOTER_SHOW_SERVICES, 0) != 0;
+        boolean settingsButtonVisible = Settings.System.getInt(mContext.getContentResolver(),
+                Settings.System.QSFOOTER_SHOW_SETTINGS, 1) != 0;
         mMultiUserSwitch.setVisibility(showUserSwitcher(isDemo) ? View.VISIBLE : View.INVISIBLE);
         mEdit.setVisibility(isDemo || !mExpanded ? View.INVISIBLE : View.VISIBLE);
-        mSettingsButton.setVisibility(isDemo || !mExpanded ? View.INVISIBLE : View.VISIBLE);
+        mSettingsButton.setVisibility(isDemo || settingsButtonVisible ? View.VISIBLE : View.GONE);
+        mRunningServicesButton.setVisibility(servicesButtonVisible ? (!isDemo && mExpanded ? View.VISIBLE : View.INVISIBLE) : View.GONE);
     }
 
     private boolean showUserSwitcher(boolean isDemo) {
