3ef68903773280011c9b1083bbb2c76777f9d6d7
mydongistiny <jaysonedson@gmail.com>
2018-06-17 17:44:52 -0700

    QS: SystemUI: Add On-The-Go Tile
    Signed-off-by: phenomenall <venkysaihot@gmail.com>

--- packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
+++ packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
@@ -38,6 +38,7 @@ import com.android.systemui.qs.tiles.LocationTile;
 import com.android.systemui.qs.tiles.LteTile;
 import com.android.systemui.qs.tiles.NfcTile;
 import com.android.systemui.qs.tiles.NightDisplayTile;
+import com.android.systemui.qs.tiles.OnTheGoTile;
 import com.android.systemui.qs.tiles.RotationLockTile;
 import com.android.systemui.qs.tiles.SoundTile;
 import com.android.systemui.qs.tiles.ScreenshotTile;
@@ -107,6 +108,8 @@ public class QSFactoryImpl implements QSFactory {
                 return new SoundTile(mHost);
             case "screenshot":
                 return new ScreenshotTile(mHost);
+            case "onthego":
+                return new OnTheGoTile(mHost);
         }
 
         // Intent tiles.
