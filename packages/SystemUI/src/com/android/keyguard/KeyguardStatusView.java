dd4eb3e9f54642acd48d4a6716ac934a066021ad
SuperDroidBond <superdroidbond@yahoo.com>
2018-10-24 18:10:02 +0530

    Welcome Back Lockscreen Item Colors [1/2]

--- packages/SystemUI/src/com/android/keyguard/KeyguardStatusView.java
+++ packages/SystemUI/src/com/android/keyguard/KeyguardStatusView.java
@@ -122,6 +122,9 @@ public class KeyguardStatusView extends GridLayout implements
 		refreshLockDateFont();
 		refreshclocksize();
 		refreshdatesize();
+                updateClockColor();
+                updateClockDateColor();
+                updateOwnerInfoColor();
             }
         }
 
@@ -144,7 +147,10 @@ public class KeyguardStatusView extends GridLayout implements
 	    refreshLockDateFont();
 	    refreshclocksize();
 	    refreshdatesize();
-        }
+            updateClockColor();
+            updateClockDateColor();
+            updateOwnerInfoColor();
+	}
 
         @Override
         public void onLogoutEnabledChanged() {
@@ -244,6 +250,9 @@ public class KeyguardStatusView extends GridLayout implements
 	refreshLockDateFont();
 	refreshclocksize();
 	refreshdatesize();
+        updateClockColor();
+        updateClockDateColor();
+        updateOwnerInfoColor();
     }
 
     /**
@@ -443,6 +452,36 @@ public class KeyguardStatusView extends GridLayout implements
     private int getLockDateSize() {
         return Settings.System.getInt(mContext.getContentResolver(),
                 Settings.System.LOCKDATE_FONT_SIZE, 14);
+    }
+
+    private void updateClockColor() {
+        ContentResolver resolver = getContext().getContentResolver();
+        int color = Settings.System.getInt(resolver,
+                Settings.System.LOCKSCREEN_CLOCK_COLOR, 0xFFFFFFFF);
+
+        if (mClockView != null) {
+            mClockView.setTextColor(color);
+        }
+    }
+
+    private void updateClockDateColor() {
+        ContentResolver resolver = getContext().getContentResolver();
+        int color = Settings.System.getInt(resolver,
+                Settings.System.LOCKSCREEN_CLOCK_DATE_COLOR, 0xFFFFFFFF);
+
+        if (mKeyguardSlice != null) {
+            mKeyguardSlice.setTextColor(color);
+       	}
+    }
+
+    private void updateOwnerInfoColor() {
+        ContentResolver resolver = getContext().getContentResolver();
+        int color = Settings.System.getInt(resolver,
+                Settings.System.LOCKSCREEN_OWNER_INFO_COLOR, 0xFFFFFFFF);
+
+        if (mOwnerInfo != null) {
+            mOwnerInfo.setTextColor(color);
+        }
     }
 
     private void refreshFormat() {
@@ -1246,9 +1285,6 @@ public class KeyguardStatusView extends GridLayout implements
 
         final int blendedTextColor = ColorUtils.blendARGB(mTextColor, Color.WHITE, mDarkAmount);
         updateDozeVisibleViews();
-        mKeyguardSlice.setDarkAmount(mDarkAmount);
-        mClockView.setTextColor(blendedTextColor);
-        mClockSeparator.setBackgroundColor(blendedTextColor);
         mCustomClockView.setDark(dark);
 		mSpideyClockView.setDark(dark);
 		mCustomNumClockView.setDark(dark);
