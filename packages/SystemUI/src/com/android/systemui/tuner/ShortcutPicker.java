sanchitrohilla/abc_frameworks_base

Fix wrong actionbar title sticking around after exit
If you entered either left or right shortcut fragments and exited out of them,
the old title (either Left shortcut or Right shortcut) would stick around. This
is just janky and should never happen.

Steps to reproduce
- Enter left or right shortcut fragment
- Exit out

Change-Id: I31078c4fdecb905abf6b5a19d41222c5a161f7be

packages/SystemUI/src/com/android/systemui/tuner/ShortcutPicker.java
@@ -134,6 +134,7 @@ public class ShortcutPicker extends PreferenceFragment implements Tunable {
     public void onDestroy() {
         super.onDestroy();
         mTunerService.removeTunable(this);
+        getActivity().setTitle(R.string.lockscreen_tuner_activity);
     }
 
     @Override

StagOS/android_frameworks_base_p

packages/SystemUI/src/com/android/systemui/tuner/ShortcutPicker.java
@@ -134,6 +134,7 @@ public class ShortcutPicker extends PreferenceFragment implements Tunable {
     public void onDestroy() {
         super.onDestroy();
         mTunerService.removeTunable(this);
+        getActivity().setTitle(R.string.lockscreen_tuner_title);
     }
 
     @Override