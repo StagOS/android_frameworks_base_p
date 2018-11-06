2e5ebb01214e9cfc7baa49b8dcc7243409ee18da
phenomenall <venkysaihot@gmail.com>
2018-11-06 10:29:33 +0000

    Bring up Stag Tones [1/2]
    All instrumentals - No copyright infringement intended
    
    * This Screwd Aosp team for this idea and some tunes
    
    Signed-off-by: phenomenall <venkysaihot@gmail.com>

---
+++ data/sounds/StagAudio.mk
@@ -0,0 +1,21 @@
+# Copyright 2018-2019 STAG OS
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); 
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software 
+# distributed under the  License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,  either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+LOCAL_PATH := frameworks/base/data/sounds/
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)stag/ringtones/June27.ogg:system/media/audio/ringtones/June27.ogg \
+    $(LOCAL_PATH)stag/ringtones/Mann.ogg:system/media/audio/ringtones/Mann.ogg \
+    $(LOCAL_PATH)stag/ringtones/stag.ogg:system/media/audio/ringtones/stag.ogg \
+    $(LOCAL_PATH)stag/ringtones/iphone.ogg:system/media/audio/ringtones/iphone.ogg \
