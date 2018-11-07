29195b2332c3410f2680317572ee1c445620727c
Roman Birg <romanbirg@gmail.com>
2018-11-07 20:59:20 +0100

    Add isPackageInstalled boolean

--- core/java/com/android/internal/util/stag/StagUtils.java
+++ core/java/com/android/internal/util/stag/StagUtils.java
@@ -20,7 +20,9 @@ import android.app.ActivityManager;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
+import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
+import android.content.pm.PackageManager.NameNotFoundException;
 import android.hardware.input.InputManager;
 import android.os.Handler;
 import android.os.Looper;
@@ -119,6 +121,26 @@ public class StagUtils {
             }
         }
     }
+
+    // Check to see if a package is installed
+    public static boolean isPackageInstalled(Context context, String pkg, boolean ignoreState) {
+        if (pkg != null) {
+            try {
+                PackageInfo pi = context.getPackageManager().getPackageInfo(pkg, 0);
+                if (!pi.applicationInfo.enabled && !ignoreState) {
+                    return false;
+                }
+            } catch (NameNotFoundException e) {
+                return false;
+            }
+        }
+         return true;
+    }
+     public static boolean isPackageInstalled(Context context, String pkg) {
+        return isPackageInstalled(context, pkg, true);
+    }
+
+
 
     /**
      * @hide
