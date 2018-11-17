3f00bf10a2354afe98f6c390731f57fe503e2578
Alex Cruz <alex@dirtyunicorns.com>
2018-11-17 19:41:05 +0530

    Power Menu animations [1/2]
    SDB: Bring it back from the Dead
    
    Fw_base - Rotation anim to Power and reboot menu [1/2]

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5168,6 +5168,13 @@ public final class Settings {
         public static final String CARBON_CUSTOM_GESTURE_PACKAGE_DOWN = "carbon_custom_gesture_package_down";
 
         /**
+         * Whether to display set of animations when prompting the power menu
+         *
+         * @hide
+         */
+        public static final String POWER_MENU_ANIMATIONS = "power_menu_animations";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
