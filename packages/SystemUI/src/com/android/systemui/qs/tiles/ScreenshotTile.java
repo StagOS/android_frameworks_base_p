daaa69d46182fc5197e11b6a490ba9d9161d8992
phenomenall <venkysaihot@gmail.com>
2018-12-14 14:48:23 +0100

    tmp

--- packages/SystemUI/src/com/android/systemui/qs/tiles/ScreenshotTile.java
+++ packages/SystemUI/src/com/android/systemui/qs/tiles/ScreenshotTile.java
@@ -21,7 +21,7 @@ import android.os.UserHandle;
 import android.provider.Settings;
 import android.service.quicksettings.Tile;
 
-import com.android.internal.util.kcuf.KCUFUtils;
+import com.android.internal.util.stag.STAGUtils;
 import com.android.internal.logging.nano.MetricsProto.MetricsEvent;
 import com.android.systemui.qs.QSHost;
 import com.android.systemui.plugins.qs.QSTile.BooleanState;
@@ -39,7 +39,7 @@ public class ScreenshotTile extends QSTileImpl<BooleanState> {
 
     @Override
     public int getMetricsCategory() {
-        return MetricsEvent.KCUF_SETTINGS;
+        return MetricsEvent.HORNS;
     }
 
     @Override
@@ -65,7 +65,7 @@ public class ScreenshotTile extends QSTileImpl<BooleanState> {
         try {
              Thread.sleep(1000); //1s
         } catch (InterruptedException ie) {}
-        KCUFUtils.takeScreenshot(mRegion ? false : true);
+        STAGUtils.takeScreenshot(mRegion ? false : true);
     }
 
     @Override
