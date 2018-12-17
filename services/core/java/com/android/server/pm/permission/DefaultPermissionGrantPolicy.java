BootleggersROM/frameworks_base

Fix setupwizard FC
Change-Id: I8c28518ea1d146671f07cfb20268a746cce40f24

services/core/java/com/android/server/pm/DefaultPermissionGrantPolicy.java
@@ -316,7 +316,7 @@ final class DefaultPermissionGrantPolicy {
                     mService.mSetupWizardPackage);
             if (setupPackage != null
                     && doesPackageSupportRuntimePermissions(setupPackage)) {
-                grantRuntimePermissionsLPw(setupPackage, PHONE_PERMISSIONS, userId);
+                grantRuntimePermissionsLPw(setupPackage, PHONE_PERMISSIONS, true, userId);
                 grantRuntimePermissionsLPw(setupPackage, CONTACTS_PERMISSIONS, userId);
                 grantRuntimePermissionsLPw(setupPackage, LOCATION_PERMISSIONS, userId);
                 grantRuntimePermissionsLPw(setupPackage, CAMERA_PERMISSIONS, userId);

StagOS/android_frameworks_base_p

Signed-off-by: Shekhawat2 <anandsingh215@yahoo.com>
Signed-off-by: phenomenall <venkysaihot@gmail.com>

services/core/java/com/android/server/pm/permission/DefaultPermissionGrantPolicy.java
@@ -382,7 +382,7 @@ public final class DefaultPermissionGrantPolicy {
         PackageParser.Package setupPackage = getSystemPackage(setupWizardPackageName);
         if (setupPackage != null
                 && doesPackageSupportRuntimePermissions(setupPackage)) {
-            grantRuntimePermissions(setupPackage, PHONE_PERMISSIONS, userId);
+            grantRuntimePermissions(setupPackage, PHONE_PERMISSIONS, true, userId);
             grantRuntimePermissions(setupPackage, CONTACTS_PERMISSIONS, userId);
             grantRuntimePermissions(setupPackage, LOCATION_PERMISSIONS, userId);
             grantRuntimePermissions(setupPackage, CAMERA_PERMISSIONS, userId);