6143b2516b7402fdce9bb578083b80970e076015
phenomenall <venkysaihot@gmail.com>
2019-01-08 07:31:59 +0530

    my bad

--- packages/SystemUI/src/com/android/systemui/qs/QSPanel.java
+++ packages/SystemUI/src/com/android/systemui/qs/QSPanel.java
@@ -870,10 +870,6 @@ public class QSPanel extends LinearLayout implements Tunable, Callback, Brightne
         }
     }
 
-    public boolean isBrightnessViewBottom() {
-        return mBrightnessBottom;
-    }
-
     public int getNumColumns() {
         return mTileLayout.getNumColumns();
     }
