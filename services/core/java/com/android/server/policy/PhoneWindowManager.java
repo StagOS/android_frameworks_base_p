LineageOS/android_frameworks_base

fw/b: Return a KeyEvent instead of a boolean in KeyHandler
* Allows handlers to modify the event before sending it off
  to another KeyHandler class, to handle things like rotation

Change-Id: I481107e050f6323c5897260a5d241e64b4e031ac

services/core/java/com/android/server/policy/PhoneWindowManager.java
@@ -4219,7 +4219,8 @@ public class PhoneWindowManager implements WindowManagerPolicy {
                 if (DEBUG_INPUT) {
                     Log.d(TAG, "Dispatching key event " + event + " to handler " + handler);
                 }
-                if (handler.handleKeyEvent(event)) {
+                event = handler.handleKeyEvent(event);
+                if (event == null) {
                     return true;
                 }
             } catch (Exception e) {

StagOS/android_frameworks_base_p

services/core/java/com/android/server/policy/PhoneWindowManager.java
@@ -7050,7 +7050,8 @@ public class PhoneWindowManager implements WindowManagerPolicy {
                     result &= ~ACTION_PASS_TO_USER;
                     return result;
                 }
-                if (mDeviceKeyHandler.handleKeyEvent(event)) {
+                event = mDeviceKeyHandler.handleKeyEvent(event);
+                if (event == null) {
                     result &= ~ACTION_PASS_TO_USER;
                     return result;
                 }