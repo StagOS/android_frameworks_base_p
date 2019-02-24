d25b2f3953b7b128c49d0b8b6c8d1335fddda5c7
phenomenall <venkysaihot@gmail.com>
2019-02-24 06:24:06 +0100

    Derp (Double declaration )

--- services/core/java/com/android/server/am/ActivityRecord.java
+++ services/core/java/com/android/server/am/ActivityRecord.java
@@ -2099,15 +2099,6 @@ final class ActivityRecord extends ConfigurationContainer implements AppWindowCo
         }
     }
 
-    public int isAppInfoGame() {
-        int isGame = 0;
-        if (appInfo != null) {
-            isGame = (appInfo.category == ApplicationInfo.CATEGORY_GAME ||
-                      (appInfo.flags & ApplicationInfo.FLAG_IS_GAME) == ApplicationInfo.FLAG_IS_GAME) ? 1 : 0;
-        }
-        return isGame;
-    }
-
     @Override
     public void onStartingWindowDrawn(long timestamp) {
         synchronized (service) {
