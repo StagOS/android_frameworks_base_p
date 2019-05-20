f20f754081b0b76a5d70d3313bdbcefc4646e7c9
phenomenall <venkysaihot@gmail.com>
2019-05-20 08:32:41 +0200

    Revert "Full nav gesture: Poor Hack to Get Everything working"
    This reverts commit 09eb12b2d3e7b48cf7aae117e6f49ce0b6cfb7ef.

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/NavigationBarFragment.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/NavigationBarFragment.java
@@ -142,7 +142,6 @@ Navigator.OnVerticalChangedListener, KeyguardMonitor.Callback, NotificationMedia
     public static final int NAVIGATION_MODE_FLING = 2;
 
     protected Navigator mNavigationBarView = null;
-    protected NavigationBarView mOldNavBarView = null;
 
     protected AssistManager mAssistManager;
 
@@ -1161,8 +1160,8 @@ Navigator.OnVerticalChangedListener, KeyguardMonitor.Callback, NotificationMedia
         public void onChange(boolean selfChange) {
             NavigationBarFragment.this.updateAccessibilityServicesState(mAccessibilityManager);
             NavigationBarFragment.this.setFullGestureMode();
-            if (mOldNavBarView != null) {
-                mOldNavBarView.updateNavButtonIcons();
+            if (mNavigationBarView != null) {
+                mNavigationBarView.updateNavButtonIcons();
             }
         }
     }
@@ -1184,10 +1183,10 @@ Navigator.OnVerticalChangedListener, KeyguardMonitor.Callback, NotificationMedia
         } catch (Settings.SettingNotFoundException e) {
         }
 	//TODO: We need to get this done in sometime or find a way for the same
-        mFullGestureMode = mOverviewProxyService.shouldShowSwipeUpUI() && fullModeEnabled;
-        if (mOldNavBarView != null) {
-            mOldNavBarView.setFullGestureMode(mFullGestureMode, dt2sEnabled);
-        }
+        /*mFullGestureMode = mOverviewProxyService.shouldShowSwipeUpUI() && fullModeEnabled;
+        if (mNavigationBarView != null) {
+            NavigationBarView.setFullGestureMode(mFullGestureMode, dt2sEnabled);
+        }*/
     }
 
     private final Stub mRotationWatcher = new Stub() {
