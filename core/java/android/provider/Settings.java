6fa5d5bfd5d1e7b984091d1d7ead17792954948b
phenomenall <venkysaihot@gmail.com>
2019-05-20 08:51:35 +0200

    Revert "Full nav gesture: home action on the whole bar and doubletap2sleep [1/2]"
    This reverts commit d568257f6c5bb7fcfc411d59f8708241a90e4d51.

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5026,10 +5026,6 @@ public final class Settings {
          */
         public static final String FULL_GESTURE_NAVBAR = "full_gesture_navbar";
 
-        /**
-         * @hide
-         */
-        public static final String FULL_GESTURE_NAVBAR_DT2S = "full_gesture_navbar_dt2s";
 
         /**
          * @hide
