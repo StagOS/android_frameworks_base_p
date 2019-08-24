1cecb6384144304bbf8269e6afeb2ac66759310d
vjspranav <pranavasri@live.in>
2019-08-24 11:16:07 +0200

    Remove Night light QS Tile

--- packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
+++ packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
@@ -43,7 +43,6 @@ import com.android.systemui.qs.tiles.LiveDisplayTile;
 import com.android.systemui.qs.tiles.LocationTile;
 import com.android.systemui.qs.tiles.LteTile;
 import com.android.systemui.qs.tiles.NfcTile;
-import com.android.systemui.qs.tiles.NightDisplayTile;
 import com.android.systemui.qs.tiles.OnTheGoTile;
 import com.android.systemui.qs.tiles.ReadingModeTile;
 import com.android.systemui.qs.tiles.RotationLockTile;
@@ -111,8 +110,6 @@ public class QSFactoryImpl implements QSFactory {
                 return new BatterySaverTile(mHost);
             case "saver":
                 return new DataSaverTile(mHost);
-            case "night":
-                return new NightDisplayTile(mHost);
             case "nfc":
                 return new NfcTile(mHost);
             case "lte":
